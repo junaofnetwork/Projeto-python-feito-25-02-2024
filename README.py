4beb5ca61c99fb7504d245a041413c9bcaeac957
João eduardo <160231914+junaofnetwork@users.noreply.github.com>
2024-02-25 15:45:17 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+# isso é umnumero irteiro/int <class 'int'>
+print(type(10))
+# isso é um texto/str <class 'str'>
+print(type("oi"))
+# isso é numero quebrado/float <class 'float'>
+print(type(5.5))
+# isso é variavel tipo true/false/bool<class 'bool'>
+print(type(True))
+print(type(1j))
